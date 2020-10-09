b042e2de96b47fd7cbd1fc9a5981b1a749f15d6b
AdamW <58723605+KwasikSasik@users.noreply.github.com>
2020-10-09 11:00:06 +0200

    updated

--- venv/cw1/Cw1.py
+++ venv/cw1/Cw1.py
@@ -35,7 +35,7 @@ def printer(D1,D2,D3,D4):
     print(D3)
     print(D4)
     print(" ")
-
+print("Podstawowe pliki:")
 printer(D1,D2,D3,D4)
 
 D1 = cutter(D1)
@@ -43,6 +43,7 @@ D2 = cutter(D2)
 D3 = cutter(D3)
 D4 = cutter(D4)
 
+print("Wycięte tagi:")
 printer(D1,D2,D3,D4)
 
 allWords = D1 + D2 + D3 + D4
@@ -50,6 +51,7 @@ allWords = set(allWords)
 allWords = list(allWords)
 allWords.sort()
 
+print("tablica tagów:")
 print(allWords)
 print(" ")
 print("slowo:           D1 D2 D3 D4")
@@ -69,19 +71,22 @@ for i in range(len(allWords)):
     tab[i][0] = "{:<12}".format(tab[i][0])
     print(tab[i])
 
+print(" ")
 vD1 = vCacl(tab,1).real
 vD2 = vCacl(tab,2).real
 vD3 = vCacl(tab,3).real
 vD4 = vCacl(tab,4).real
-
+print("długości")
 print("vD1 = " + str(vD1))
 print("vD2 = " + str(vD2))
 print("vD3 = " + str(vD3))
 print("vD4 = " + str(vD4))
+print(" ")
 
+print(" Tablica po normalizacji")
 for j in range(len(allWords)):
-    tab[j][1] ="{:<4}".format( round(tab[j][1]/vD1 , 2))
-    tab[j][2] ="{:<4}".format( round(tab[j][2]/vD2 , 2))
-    tab[j][3] ="{:<4}".format( round(tab[j][3]/vD3 , 2))
-    tab[j][4] ="{:<4}".format( round(tab[j][4]/vD4 , 2))
+    tab[j][1] ="{:<6}".format( round( tab[j][1] * (tab[j][1]/vD1) , 4))
+    tab[j][2] ="{:<6}".format( round( tab[j][2] * (tab[j][2]/vD2) , 4))
+    tab[j][3] ="{:<6}".format( round( tab[j][3] * (tab[j][3]/vD3) , 4))
+    tab[j][4] ="{:<6}".format( round( tab[j][4] * (tab[j][4]/vD4) , 4))
     print(tab[j])
