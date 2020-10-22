4c95b0aa3a26114be990d1bfd6c46878ca4e363a
AdamW <58723605+KwasikSasik@users.noreply.github.com>
2020-10-22 10:32:10 +0200

    End of Exc

--- venv/cw2/Cw2.py
+++ venv/cw2/Cw2.py
@@ -46,7 +46,7 @@ def printer(D1,D2,D3,D4):
 ########################################################################################################################
 
 D1 = "bazy relacyjne, bazy tekstowe, bazy inne"
-D2 = "bazy danych: przykady, zastosowania"
+D2 = "bazy danych: przyklady, zastosowania"
 D3 = "bazy danych - zalety; bazy danych - wady"
 D4 = "skladowanie danych"
 Z  = "bazy danych"
@@ -59,7 +59,7 @@ D1 = cutter(D1)
 D2 = cutter(D2)
 D3 = cutter(D3)
 D4 = cutter(D4)
-
+Z = cutter(Z)
 print("Wycięte tagi:")
 printer(D1,D2,D3,D4)
 
@@ -72,11 +72,11 @@ print("tablica tagów:")
 print(allWords)
 print(" ")
 print("Czestowtliwosc wystepowania w pliku:")
-print("slowo:           D1 D2 D3 D4")
+print("slowo:           D1 D2 D3 D4  Z")
 
-tab  = [[[0] for i in range(5)] for i in range(len(allWords))]
-tab2 = [[[0] for j in range(5)] for j in range(len(allWords))]
-tab3 = [[[0] for j in range(5)] for j in range(len(allWords))]
+tab  = [[[0] for i in range(6)] for i in range(len(allWords))]
+tab2 = [[[0] for j in range(6)] for j in range(len(allWords))]
+tab3 = [[[0] for j in range(6)] for j in range(len(allWords))]
 for i in range(len(allWords)):
 
     word = allWords[i]
@@ -87,16 +87,19 @@ for i in range(len(allWords)):
     tab[i][2] = containSQty(word, D2)
     tab[i][3] = containSQty(word, D3)
     tab[i][4] = containSQty(word, D4)
+    tab[i][5] = containSQty(word, Z)
 
     tab2[i][1] = containS(word, D1)
     tab2[i][2] = containS(word, D2)
     tab2[i][3] = containS(word, D3)
     tab2[i][4] = containS(word, D4)
+    tab2[i][5] = containS(word, Z)
 
     tab3[i][1] = containSQty(word, D1)
     tab3[i][2] = containSQty(word, D2)
     tab3[i][3] = containSQty(word, D3)
     tab3[i][4] = containSQty(word, D4)
+    tab3[i][5] = containSQty(word, Z)
 
     #printing formatter V
     tab[i][0] = "{:<12}".format(tab[i][0])
@@ -121,6 +124,7 @@ for j in range(len(allWords)):
     tab[j][2] ="{:<6}".format(round(   tab[j][2] * (1/vD2)    , 4))
     tab[j][3] ="{:<6}".format(round(   tab[j][3] * (1/vD3)    , 4))
     tab[j][4] ="{:<6}".format(round(   tab[j][4] * (1/vD4)    , 4))
+    tab[j][5] ="{:<6}".format(round(   tab[j][5] * (1/vD4)    , 4))
     print(tab[j])
 print(" ")
 
@@ -147,3 +151,40 @@ for j in range(len(allWords)):
     tab[j][4] ="{:<6}".format(round(   float(tab[j][4]) * (1/vD4)    , 4))
     print(tab[j])
 print(" ")
+
+
+#Podobienstwa
+pD1 = 0
+pD2 = 0
+pD3 = 0
+pD4 = 0
+for j in range(len(allWords)):
+    tab[j][1] ="{:<6}".format(round( float(tab[j][1]) * float(tab[j][5])     , 4))
+    tab[j][2] ="{:<6}".format(round( float(tab[j][2]) * float(tab[j][5])     , 4))
+    tab[j][3] ="{:<6}".format(round( float(tab[j][3]) * float(tab[j][5])     , 4))
+    tab[j][4] ="{:<6}".format(round( float(tab[j][4]) * float(tab[j][5])     , 4))
+    pD1 = pD1 + float(tab[j][1])
+    pD2 = pD2 + float(tab[j][2])
+    pD3 = pD3 + float(tab[j][3])
+    pD4 = pD4 + float(tab[j][4])
+
+
+
+print("Pliki:")
+print(D1)
+print(D2)
+print(D3)
+print(D4)
+print(" ")
+
+print("Zapytanie:")
+print(Z)
+print(" ")
+
+print("Podobienstwa:")
+print("podobieństwo zapytania Z do D1 = " + str(pD1))
+print("podobieństwo zapytania Z do D2 = " + str(pD2))
+print("podobieństwo zapytania Z do D3 = " + str(pD3))
+print("podobieństwo zapytania Z do D4 = " + str(pD4))
+
+
